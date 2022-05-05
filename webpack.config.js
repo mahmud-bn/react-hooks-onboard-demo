53c778e486f01d06b1cc1aeaf74284acbb249062
Mahmud Adeleye <mahmud@blocknative.com>
2022-05-05 11:48:29 -0600

    Integrate web3-onboard with react hooks'

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack')
+
+module.exports = {
+  resolve: {
+    alias: {
+      assert: 'assert',
+      buffer: 'buffer',
+      crypto: 'crypto-browserify',
+      http: 'stream-http',
+      https: 'https-browserify',
+      os: 'os-browserify/browser',
+      process: 'process/browser',
+      stream: 'stream-browserify',
+      util: 'util'
+    }
+  },
+  experiments: {
+    asyncWebAssembly: true
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  ]
+}
